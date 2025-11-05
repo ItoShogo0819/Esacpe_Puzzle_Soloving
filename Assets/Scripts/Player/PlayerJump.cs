3eedba3ed826083939d78a64b9ce7b0e3ce7c683
flaming8634 <flaming.8634@icloud.com>
2025-11-05 13:37:43 +0900

    [Add]腕追加

---
+++ Assets/Scripts/Player/PlayerJump.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerJump : MonoBehaviour
+{
+    Rigidbody rb;
+    public float JumpForce = 5f;
+    public bool IsGround = true;
+    public bool JumpControl = true;
+    
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
